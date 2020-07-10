8b8c252b4a75abaa6d2fc1d5c094f5c7753e9649
DryWang <yanwang.eir@gmail.com>
2020-07-10 17:58:26 +0800

    update

---
+++ hello.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import os
+os.chdir('C:\\Users\\Yan\\Downloads')
+modules = dir()
+print(modules)
--- tie.py
+++
@@ -1,3 +0,0 @@
-import numpy
-import pandas as pd
-os.chdir('C:\Users\Yan\Downloads') 
